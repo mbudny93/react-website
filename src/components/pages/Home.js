85c7161b770fe9bc12a5a0fef9cf5832aa319cf8
budnma <marcinbudny93@gmail.com>
2021-05-15 22:30:16 +0200

    footer

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 import '../../App.css'
 import BodySection from '../BodySection'
 import Cards from '../Cards'
+import Footer from '../Footer'
 
 function Home(){
     return(
         <>
             <BodySection/>
             <Cards/>
+            <Footer/>
         </>
     );
 }
